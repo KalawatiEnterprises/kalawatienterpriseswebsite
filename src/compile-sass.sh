24ff59128027358e5d514383acb65354f314fdfb
Vidhu Kant Sharma <vidhukant@protonmail.ch>
2022-06-03 18:30:55 +0530

    added backdrop image with text on homepage

--- src/compile-sass.sh
+++ src/compile-sass.sh
@@ -5,5 +5,6 @@ cmd="sass $@"
 $cmd styles/brands/brands.scss:../public/css/brands.css &
 $cmd styles/categories/categories.scss:../public/css/categories.css &
 $cmd styles/contact.scss:../public/css/contact.css &
+$cmd styles/homepage.scss:../public/css/homepage.css &
 $cmd styles/products/products.scss:../public/css/products.css &
 $cmd styles/styles.scss:../public/css/styles.css &
